e161d3606dfdad27a93d5b25a470ccad7a36afd6
Shivanii97 <136204138+Shivanii97@users.noreply.github.com>
2023-06-12 05:41:02 +0000

    This commit adds new commands for Python

--- Hello_World.py
+++ Hello_World.py
@@ -1,1 +1,17 @@
-print("Hello_World")
+""""
+How to Run a program?
+    1)Write your code with print("Hello World").  ("Hello World") is a string..
+    2))A string should be always in parantheses with double inverted comma..Ex - ("")
+    3)Run the code in the terminal. Write python <File name>. <File name> is the file in which you
+    have written your codes.
+    """
+
+print("Hello World")
+print("Good Morning Shivani")
+print("Wish you have a great day")
+print("All the very best for your python learning")
+print("I am very excited to learn python")
+print("Let's get started")
+print("Today I learnt about programming, python,modules,pip and how to run a program in python")
+
+
---
+++ Python_codes.py
@@ -0,0 +1,25 @@
+#How to add a new line after the text or how to insert a new line text?
+    #Use Escape Sequence Character
+    #\n use backslash n, you will get a new line for your txt
+
+    
+print("My name is Shivani\nand my age is 25")
+     #Ex- My name is Shivani
+         #and my age is 25
+
+print("Hi, what are you doing\nand what are your plans for the weekend")
+    #Ex- Hi, what are you doing
+        #and what are your plans for the weekend
+
+#Double quote escape sequence characters:
+# if you want to write the txt in "-" in a string
+#we will use backslash double quote; \"-\"
+    #Ex: print("Hello World, my name is \"Shivani"\ and I am learning python")
+
+print("Hello World, my name is \"shivani\"\nand my age is 25")
+    #Ex- Hello World, my name is "shivani"
+         #and my age is 25
+
+
+print("\"Good Morning Jaan\"\nwake up now\nget frshen up, get ready!")
+print("And remember one thing that \"I LOVE YOU\"")
