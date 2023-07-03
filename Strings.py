552b7a06d5843c05c7fda6bfc711490cda54e516
Shivanii97 <136204138+Shivanii97@users.noreply.github.com>
2023-07-03 15:20:10 +0000

    Learnt For loop

--- Strings.py
+++ Strings.py
@@ -2,8 +2,24 @@
 #Anything that enclose between single or double quotation marks is known as string
 name = "Harry"
 print("Hello" , name)  #Hello Harry
+
 #Indexing - 0,1,2,3,4,5...etc
 print(name[0])  #H [] square brackets is used 
+
+
+name = "Shivani"     
+print(name[0])        #S
+print(name[1])        #h
+print(name[2])        #i
+
+#Looping through the string
+#for character in name :
+#print(charcter)
+
+apple = ("Hello, how are you, what's going on, what are your plans")
+for character in apple :
+    print(character)
+
 
 #String Slicing - Lenght of a string, by using len(). To know the length of  string
 name = "Today is Monday, and it's sunny"   #output = 13(length of the string)
